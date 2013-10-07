d02c34bbeef6e95eff202c115144c5decb4e08d0
root <root@isismst.com>
2013-10-07 16:33:37 +0000

    error page

--- plugins/system/creditcards/creditcards.php
+++ plugins/system/creditcards/creditcards.php
@@ -27,7 +27,7 @@ class PlgSystemCreditcards extends JPlugin
 	 *
 	 * @since   3.0
 	 */
-	public function onAfterRoute()
+/*	public function onAfterRoute()
 	{	
 
 		
@@ -63,7 +63,7 @@ class PlgSystemCreditcards extends JPlugin
 
 	}	
 
-
+*/
 }
 
 ?>
