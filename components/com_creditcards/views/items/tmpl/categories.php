b83dfd79b2100a24ce3afcb6b6f5b137509552c2
Chris French <chris@ammonitenetworks.com>
2013-10-16 11:03:45 -0600

    adding link to form page, and menu fixes

--- components/com_creditcards/views/items/tmpl/categories.php
+++ components/com_creditcards/views/items/tmpl/categories.php
@@ -7,17 +7,16 @@ $doc->addStyleSheet('/templates/crosscliq/css/list.css');
 <div data-role="header" data-position="fixed">
 		<h1>Isis</h1>
 
-<? if (strpos(JURI::current(),'verizon') !== false) { ?>
-		<a href="#popupMenu" data-role="button" class="ui-btn-right" data-icon="bars"  data-rel="popup">Menu</a>
+<a href="#popupMenu" data-role="button" class="ui-btn-right" data-icon="bars"  data-rel="popup">Menu</a>
 <div data-role="popup" id="popupMenu" >
     <ul data-role="listview" data-inset="true" style="width:180px;" data-theme="b">
-        <li><a data-rel="popup" href="http://cards.isismst.com/verizon">Card Tool</a></li>
+        <li><a data-rel="popup" href="<?=substr(JURI::current(), 0, strrpos ( JURI::current(), '/' ));?>">Card Tool</a></li>
         <li><a data-rel="popup" href="http://demo.isismst.com">Demo</a></li>
         <li><a data-rel="popup" href="http://locator.isismst.com">Locator</a></li>
         <li><a data-rel="popup" href="http://offers.isismst.com">Offer Tool</a></li>
     </ul>
 </div>
-<? } ?>
+
 	</div>
 
 	<h3 style="text-align:center;color:#9D1D86;">
